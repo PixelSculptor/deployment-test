f0ed2ef203edf70b1d400bab7094d754b77a9074
Kacper Nadstoga <kapix007@gmail.com>
2026-06-24 14:11:27 +0200

    setup infra with cdk

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "./infra/resources/build",
+    outDir: "./aws/resources/build",
   },
 });
